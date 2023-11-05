cbe294d76d9a93ac7df128c32a6c1da066f26dd1
nisreen khattam <NISOKH7GMAIL.COM>
2023-11-05 23:52:42 +0100

    commit ff

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import Navbar from './components/Navbar';
 import Slider from './components/Slider';
 import ProductsList from './components/ProductsList';
 
+import { Routes,Route } from 'react-router-dom';
+import About from './components/About';
+
 function App() {
   return (
     <div className="App">
    <Navbar />
-   <Slider />
-   <ProductsList />
+
+   <Routes>
+    <Route path="/" element={
+              <>
+                  <Slider />
+                  <ProductsList />
+              </>
+            }> 
+    </Route>
+    <Route path="About" element={ <> <About /> </>}></Route>
+   </Routes>
+  
   
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 function Navbar(){
 
 
@@ -5,17 +6,17 @@ function Navbar(){
         <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container">
-    <a className="navbar-brand" href="/#">Logo</a>
+    <Link className="navbar-brand" to="/">Logo</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/#">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/#">about</a>
+          <Link className="nav-link" to="/About">about</Link>
         </li>
 
        
